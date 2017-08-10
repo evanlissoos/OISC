391d8f4e54a19a391705e2b95f796084e5ac4556
Evan Lissoos <evanlissoos@users.noreply.github.com>
2017-08-09 18:49:05 -0700

    Update oisc.cpp

--- oisc.cpp
+++ oisc.cpp
@@ -14,7 +14,7 @@ template <class width_t>
 oisc::oisc()
 {
 	PC = 0;
-	MEMORY = new width_t[sizeof(2 << width_t)];
+	MEMORY = new width_t[1 << (sizeof(width_t) * 8)];
 	memory_owner = true;
 }
 
@@ -32,22 +32,41 @@ oisc::~oisc()
 		free(memory);
 }
 
+void oisc::cycle()
+{
+	width_t a, b, c;
+	a = MEMORY[PC];
+	b = MEMORY[PC + 1];
+	c = MEMORY[PC + 2];
+	MEMORY[b] = MEMORY[b] - MEMORY[a];
+	PC = (MEMORY[b] > 0) ? PC + 3 : c;
+}
+
 void oisc::run(width_t start_address = 0)
 {
-
+	PC = start_address;
+	while(PC >= 0)	cycle();
 }
 
-void oisc::print_info(uint8_t size = 8)
-{
-
-}
+//Print needs to be a user implemented function
+//void oisc::print_info(uint8_t size = 8)
 
 void oisc::load_memory(width_t start_index, width_t end_index, width_t * memory)
 {
 
 }
 
+width_t * oisc::get_memory()
+{
+	return MEMORY;
+}
+
+width_t oisc::get_pc()
+{
+	return PC:
+}
+
 uint32_t oisc::get_width()
 {
-	
+	return sizeof(width_t);
 }

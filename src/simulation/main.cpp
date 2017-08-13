3e53c5a7555654cf353af8d76b43c404ba452ad8
evanlissoos <evanlissoos@gmail.com>
2017-08-13 12:59:03 -0700

    Thread functions for input and output written

--- src/simulation/main.cpp
+++ src/simulation/main.cpp
@@ -1,53 +1,11 @@
 #include "lib/interactive_oisc.h"
 #include <iostream>
 #include <string>
+#include <thread>
 using namespace std;
-
-#include <termios.h>
-class BufferToggle
-{
-	 private:
-	     struct termios t;
-
-	 public:
-
-	     /*
-	      * Disables buffered input
-	      */
-
-	     void off(void)
-	     {
-	         tcgetattr(STDIN_FILENO, &t); //get the current terminal I/O structure
-	         t.c_lflag &= ~ICANON; //Manipulate the flag bits to do what you want it to do
-	         tcsetattr(STDIN_FILENO, TSCANOW, &t); //Apply the new settings
-	     }
-
-
-	     /*
-	      * Enables buffered input
-	      */
-
-	     void on(void)
-	     {
-	         tcgetattr(STDIN_FILENO, &t); //get the current terminal I/O structure
-	         t.c_lflag |= ICANON; //Manipulate the flag bits to do what you want it to do
-	         tcsetattr(STDIN_FILENO, TSCANOW, &t); //Apply the new settings
-	     }
-};
 
 int main()
 {
-	char test;
-	BufferToggle bt;
-	test = getchar(); //waits for you to press enter before proceeding to the next instruction
-	cout << test << endl;
-	bt.off();
-	test = getchar(); //processes next instruction as soon as you type a character (no enter)
-	cout << test << endl;
-	bt.on();
-	test = getchar(); //waits for you to press enter before proceeding to the next instruction
-	cout << test << endl;
-
 	interactive_oisc * test_oisc = new interactive_oisc();
 	test_oisc->run();
 
